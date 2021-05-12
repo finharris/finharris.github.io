162764d9e8ba48b4e6a81ff3b580f9493cc6c785
finharris <mybutiskfc@gmail.com>
2021-05-12 18:52:52 +0100

    landing page update

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ function toggleNav() {
 
 hamburgerIcon.addEventListener('click', toggleNav);
 closeIcon.addEventListener('click', toggleNav);
+
+document.querySelector('.temp').addEventListener('click', e => {
+  e.preventDefault();
+  console.log('here');
+
+  toggleNav();
+});
