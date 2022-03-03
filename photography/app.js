91ca06dbeb97d81339bb86b1b88bc6e23aa0973b
finharris <mybutiskfc@gmail.com>
2022-03-03 10:35:34 +0000

    removed titles from photos and added costal turbine image

--- photography/app.js
+++ photography/app.js
@@ -1,43 +1,42 @@
 // IMAHE ENLARGING
-const images = document.querySelectorAll('.resizable-image');
+const images = document.querySelectorAll(".resizable-image");
 
 for (const image of images) {
-  image.addEventListener('click', e => {
+  image.addEventListener("click", (e) => {
     if (e.target.width <= 0.75 * window.innerWidth) {
       e.target.parentNode.scrollIntoView({
-        block: 'center',
-        behavior: 'smooth',
+        block: "center",
+        behavior: "smooth",
       });
-      e.target.classList.toggle('enlarge-image');
+      e.target.classList.toggle("enlarge-image");
     } else {
-      e.target.scrollIntoView({ block: 'center', behavior: 'smooth' });
-      e.target.classList.remove('enlarge-image');
+      e.target.scrollIntoView({ block: "center", behavior: "smooth" });
+      e.target.classList.remove("enlarge-image");
     }
   });
 
-  image.addEventListener('contextmenu', e => e.preventDefault());
+  image.addEventListener("contextmenu", (e) => e.preventDefault());
 }
 
 // HAMBURGER MENU
-const hamburgerIcon = document.querySelector('.open');
-const closeIcon = document.querySelector('.close');
-const nav = document.querySelector('.ham-menu');
+const hamburgerIcon = document.querySelector(".open");
+const closeIcon = document.querySelector(".close");
+const nav = document.querySelector(".ham-menu");
 
 function toggleNav() {
-  nav.classList.toggle('nav-shown');
-  console.log(true);
+  nav.classList.toggle("nav-shown");
 }
 
-hamburgerIcon.addEventListener('click', toggleNav);
-closeIcon.addEventListener('click', toggleNav);
+hamburgerIcon.addEventListener("click", toggleNav);
+closeIcon.addEventListener("click", toggleNav);
 
 // SCROLL DOWN ICON
-const scrollDownIcon = document.querySelector('.scroll-down');
-const container = document.querySelector('.container');
+const scrollDownIcon = document.querySelector(".scroll-down");
+const container = document.querySelector(".container");
 
-scrollDownIcon.addEventListener('click', e => {
+scrollDownIcon.addEventListener("click", (e) => {
   container.scrollBy({
-    behavior: 'smooth',
+    behavior: "smooth",
     left: 0,
     top: window.innerHeight,
   });
@@ -45,14 +44,14 @@ scrollDownIcon.addEventListener('click', e => {
 });
 
 // BACK TO TOP
-const backToTop = document.querySelector('.back-to-top');
+const backToTop = document.querySelector(".back-to-top");
 
-backToTop.addEventListener('click', e => {
+backToTop.addEventListener("click", (e) => {
   container.scrollTo({
-    behavior: 'smooth',
+    behavior: "smooth",
     left: 0,
     top: 0,
   });
 
-  console.log('test');
+  console.log("test");
 });
