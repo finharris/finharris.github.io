8e5247a5bb13f9cccdf986395db0354b08d3afbd
finharris <mybutiskfc@gmail.com>
2022-03-14 15:23:17 +0000

    made text not focus on only modifier keypress

--- projects/pages/searchEngine/script.js
+++ projects/pages/searchEngine/script.js
@@ -1,19 +1,21 @@
-const searchBox = document.querySelector('#searchBox');
-const searchButton = document.querySelector('#searchButton');
-const title = document.querySelector('h1');
+const searchBox = document.querySelector("#searchBox");
+const searchButton = document.querySelector("#searchButton");
+const title = document.querySelector("h1");
 
 function searchGoogle(query) {
   searchBox.blur();
-  return window.open('https://www.google.com/search?q=' + query);
+  return window.open("https://www.google.com/search?q=" + query);
 }
 
-searchButton.addEventListener('click', () => {
+searchButton.addEventListener("click", () => {
   return searchBox.value ? searchGoogle(searchBox.value) : false;
 });
 
-window.onkeydown = k => {
+window.onkeydown = (k) => {
+  if (k.key === "Alt" || k.key === "Control" || k.key === "Shift") return;
+
   k.keyCode === 46 && searchBox.value && document.activeElement !== searchBox
-    ? (searchBox.value = '')
+    ? (searchBox.value = "")
     : false;
 
   document.activeElement !== searchBox ? searchBox.focus() : false;
@@ -21,19 +23,19 @@ window.onkeydown = k => {
   k.keyCode === 13 && searchBox.value ? searchGoogle(searchBox.value) : false;
 };
 
-searchBox.addEventListener('focus', () => {
-  title.style.fontSize = '3em';
+searchBox.addEventListener("focus", () => {
+  title.style.fontSize = "3em";
 });
 
-searchBox.addEventListener('blur', () => {
-  title.style.fontSize = '2.6em';
+searchBox.addEventListener("blur", () => {
+  title.style.fontSize = "2.6em";
 });
 
 // dropdown
 
-const dropdownButton = document.querySelector('.dropbtn');
-const dropdownContent = document.querySelector('.dropdown-content');
+const dropdownButton = document.querySelector(".dropbtn");
+const dropdownContent = document.querySelector(".dropdown-content");
 
-dropdownButton.addEventListener('click', () => {
-  dropdownContent.classList.toggle('hidden');
+dropdownButton.addEventListener("click", () => {
+  dropdownContent.classList.toggle("hidden");
 });
