bdb9793b216ee2e8c1c40ea620e8d4860a5fbf58
finharris <mybutiskfc@gmail.com>
2022-03-24 10:14:14 +0000

    made non scroll on hamburger menu work on mobile

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -2,7 +2,7 @@ const hamburgerIcon = document.querySelector(".hamburger-icon");
 const nav = document.querySelector("nav");
 
 hamburgerIcon.addEventListener("click", () => {
-  document.body.style.overflowY = nav.classList.contains("nav-shown")
+  document.body.style.overflow = nav.classList.contains("nav-shown")
     ? "visible"
     : "hidden";
   nav.classList.toggle("nav-shown");
@@ -13,6 +13,6 @@ const navItems = document.querySelectorAll(".nav-item > a");
 for (const item of navItems) {
   item.addEventListener("click", () => {
     nav.classList.remove("nav-shown");
-    document.body.style.overflowY = "visible";
+    document.body.style.overflow = "visible";
   });
 }
