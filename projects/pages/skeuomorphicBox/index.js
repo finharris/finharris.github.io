498f3043169aa124fdc035009601ddce4b0f514d
finharris <mybutiskfc@gmail.com>
2022-03-25 11:00:20 +0000

    made skeu box page not have scroll and work on space press

--- projects/pages/skeuomorphicBox/index.js
+++ projects/pages/skeuomorphicBox/index.js
@@ -1,5 +1,11 @@
 const button = document.querySelector(".button");
 
+document.addEventListener("keypress", (e) => {
+  if (e.code === "Space") {
+    button.classList.toggle("button-pressed");
+  }
+});
+
 button.addEventListener("click", () => {
   button.classList.toggle("button-pressed");
 });
